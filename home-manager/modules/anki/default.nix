5c593369b01b099a90f9571e310e3096b59f38e7
midboob <edwarddan72@gmail.com>
2025-11-07 05:11:17 -0800

    feat: ✨ added anki syncKey

--- home-manager/modules/anki/default.nix
+++ home-manager/modules/anki/default.nix
@@ -1,9 +1,20 @@
-{ lib, pkgs, ... }: {
-	stylix.targets.anki.enable = true;
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  stylix.targets.anki.enable = true;
 
-	programs.anki = {
-		enable = true;
+  programs.anki = {
+    enable = true;
+    sync = {
+      autoSync = true;
+      keyFile = "/home/edward/.dotnix/home-manager/modules/anki/syncKey";
+      username = "edwarddan72@gmail.com";
+    };
 
-		addons = [ pkgs.ankiAddons.review-heatmap ];
-	};
+    addons = with pkgs; [
+      ankiAddons.review-heatmap
+    ];
+  };
 }
