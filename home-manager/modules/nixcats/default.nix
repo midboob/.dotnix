853c930f57b31f455deeeb8507f8504e678d6b87
midboob <edwarddan72@gmail.com>
2025-11-10 12:40:53 -0800

    feat: ✨ changed some stuff in nixcats

--- home-manager/modules/nixcats/default.nix
+++ home-manager/modules/nixcats/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, inputs, ... }: let
+{
+  config,
+  lib,
+  inputs,
+  ...
+}: let
   utils = inputs.nixCats.utils;
 in {
   imports = [
@@ -13,19 +18,31 @@ in {
       # this will add the overlays from ./overlays and also,
       # add any plugins in inputs named "plugins-pluginName" to pkgs.neovimPlugins
       # It will not apply to overall system, just nixCats.
-      addOverlays = /* (import ./overlays inputs) ++ */ [
-        (utils.standardPluginOverlay inputs)
-      ];
+      addOverlays =
+        /*
+        (import ./overlays inputs) ++
+        */
+        [
+          (utils.standardPluginOverlay inputs)
+        ];
       # see the packageDefinitions below.
       # This says which of those to install.
-      packageNames = [ "myHomeModuleNvim" ];
+      packageNames = ["myHomeModuleNvim"];
 
       luaPath = ./.;
 
       # the .replace vs .merge options are for modules based on existing configurations,
       # they refer to how multiple categoryDefinitions get merged together by the module.
       # for useage of this section, refer to :h nixCats.flake.outputs.categories
-      categoryDefinitions.replace = ({ pkgs, settings, categories, extra, name, mkPlugin, ... }@packageDef: {
+      categoryDefinitions.replace = {
+        pkgs,
+        settings,
+        categories,
+        extra,
+        name,
+        mkPlugin,
+        ...
+      } @ packageDef: {
         # to define and use a new category, simply add a new list to a set here,
         # and later, you will include categoryname = true; in the set you
         # provide when you build the package using this builder function.
@@ -113,7 +130,7 @@ in {
         # shared libraries to be added to LD_LIBRARY_PATH
         # variable available to nvim runtime
         sharedLibraries = {
-          general = with pkgs; [ ];
+          general = with pkgs; [];
         };
 
         # environmentVariables:
@@ -138,13 +155,17 @@ in {
           #   '' --set CATTESTVAR2 "It worked again!"''
           # ];
         };
-      });
+      };
 
       # see :help nixCats.flake.outputs.packageDefinitions
       packageDefinitions.replace = {
         # These are the names of your packages
         # you can include as many as you wish.
-        myHomeModuleNvim = {pkgs, name, ... }: {
+        myHomeModuleNvim = {
+          pkgs,
+          name,
+          ...
+        }: {
           # they contain a settings set defined above
           # see :help nixCats.flake.outputs.settings
           settings = {
@@ -154,7 +175,7 @@ in {
             # unwrappedCfgPath = "/path/to/here";
             # IMPORTANT:
             # your alias may not conflict with your other packages.
-            aliases = [ "vim" "homeVim" "nvim" ];
+            aliases = ["vim" "homeVim" "nvim"];
             # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
             hosts.python3.enable = true;
             hosts.node.enable = true;
