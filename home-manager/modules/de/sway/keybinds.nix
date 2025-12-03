ec4382bc99b507e2cc5b34901f880387677a09ec
midboob <edwarddan72@gmail.com>
2025-12-03 15:29:24 -0800

    feat: ✨ added screenshot.sh script to sway

--- home-manager/modules/de/sway/keybinds.nix
+++ home-manager/modules/de/sway/keybinds.nix
@@ -17,7 +17,27 @@
     # };
     #
     keybindings = {
+      # Basic
+      "Mod4+Q" = "kill";
+      "Mod4+V" = "floating toggle";
+      "Mod4+F" = "fullscreen toggle";
+
+      # Apps
       "Mod4+Return" = "exec ${pkgs.foot}/bin/foot";
+      "Mod4+B" = "exec ${pkgs.brave}/bin/brave";
+      "Mod4+E" = "exec ${pkgs.foot}/bin/foot -e ${pkgs.yazi}/bin/yazi";
+      "Mod4+Shift+E" = "exec ${pkgs.nautilus}/bin/nautilus";
+      "Mod4+M" = "exec spotify";
+      "Mod4+O" = "exec ${pkgs.obsidian}/bin/obsidian";
+      "Mod4+N" = "exec ${pkgs.foot}/bin/foot -e nvim";
+      "Mod4+Shift+B" = "exec ${pkgs.foot}/bin/foot -e bluetui";
+      "Mod4+Shift+N" = "exec ${pkgs.swaynotificationcenter}/bin/swaync-client -t -sw";
+      "Mod4+Escape" = "exec wlogout";
+      "Mod4+Shift+W" = "exec rofi-wallpaper";
+
+      # Screenshots
+      "Mod4+Shift+S" = "exec ${./screenshot.sh}";
+      "Print" = "exec ${pkgs.grim}/bin/grim - | ${pkgs.wl-clipboard}/bin/wl-copy && ${pkgs.wl-clipboard}/bin/wl-paste > ~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png";
     };
   };
 }
