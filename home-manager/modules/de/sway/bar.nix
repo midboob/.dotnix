404ab55cb349cf5d1d6c43892c5b5f458fa65753
midboob <edwarddan72@gmail.com>
2025-12-03 22:53:07 -0800

    feat: ✨ completed bar config for sway

--- home-manager/modules/de/sway/bar.nix
+++ home-manager/modules/de/sway/bar.nix
@@ -1,5 +1,23 @@
-{pkgs, ...}: {
-  wayland.windowManager.sway.config.bars = {
-    position = "top";
-  };
+{
+  pkgs,
+  config,
+  ...
+}: let
+  swayStatus = pkgs.writeShellScript "sway-status" ''
+    while :; do
+      date +"%a %b %d  %l:%M %p"
+      sleep 1
+    done
+  '';
+in {
+  wayland.windowManager.sway.config.bars = [
+    (
+      {
+        position = "top";
+
+        statusCommand = "${swayStatus}";
+      }
+      // config.stylix.targets.sway.exportedBarConfig
+    )
+  ];
 }
