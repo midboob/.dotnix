4511ed61c359984a26df774cf79b230f6c362078
midboob <edwarddan72@gmail.com>
2025-12-10 14:24:46 -0800

    feat: ✨ removed nvconfig file

--- home-manager/modules/nixcats/init.lua
+++ home-manager/modules/nixcats/init.lua
@@ -23,21 +23,18 @@ require('plugins')
 -- }
 --
 -- Base46: compile + apply theme (including chadwal)
-local ok_base46, base46 = pcall(require, "base46")
-if ok_base46 then
-  base46.load_all_highlights()
-end
+ for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+   dofile(vim.g.base46_cache .. v)
+ end
 
 -- Pywal / Matugen watcher: runs in background and sends SIGUSR1 to nvim
 os.execute("python ~/.dotnix/home-manager/modules/nixcats/pywal/chadwal.py &> /dev/null &")
 
--- When the watcher sends SIGUSR1, re-run base46 so new colors apply
-vim.api.nvim_create_autocmd("Signal", {
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("Signal", {
   pattern = "SIGUSR1",
   callback = function()
-    local ok, b46 = pcall(require, "base46")
-    if ok then
-      b46.load_all_highlights()
-    end
-  end,
+    require('nvchad.utils').reload()
+  end
 })
--- home-manager/modules/nixcats/lua/plugins/init.lua
+++ home-manager/modules/nixcats/lua/plugins/init.lua
@@ -28,7 +28,8 @@ extend(require('plugins.specs.typst-preview'))
 extend(require('plugins.specs.mini'))
 extend(require('plugins.specs.fidget'))
 extend(require('plugins.specs.bullet'))
-extend(require('plugins.specs.nvconfig'))
+-- extend(require('plugins.specs.nvconfig'))
+extend(require('plugins.specs.chadrc'))
 
 
 require('lze').load(specs)
