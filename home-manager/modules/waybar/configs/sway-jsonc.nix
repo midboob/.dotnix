5572c07d4227a4de76c18abf1e03515f065e244a
midboob <edwarddan72@gmail.com>
2025-12-16 18:19:49 -0800

    feat: ✨ config

---
+++ home-manager/modules/waybar/configs/sway-jsonc.nix
@@ -0,0 +1,164 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  xdg.configFile."waybar/sway.jsonc".text = ''
+    {
+      "name": "sway",
+
+      "layer": "top",
+      "height": 20,
+      "spacing": 4,
+      "margin-top": 0,
+      "margin-right": 0,
+      "margin-left": 0,
+
+      "modules-left": [
+        "sway/workspaces",
+        "sway/mode"
+      ],
+      "modules-right": [
+        "tray",
+        "network",
+        "disk",
+        "cpu",
+        "custom/cpu_temp",
+        "backlight",
+        "custom/memory",
+        "pulseaudio",
+        "battery",
+        "clock"
+      ],
+
+      // Modules configuration
+      "sway/workspaces": {
+        "disable-scroll": true,
+        "active-only": false,
+        "all-outputs": true,
+        "warp-on-scroll": false,
+        "format": "{index}"
+      },
+
+      "sway/mode": {
+        "format": " {}",
+        "max-length": 50
+      },
+
+      "tray": {
+        "icon-size": 14,
+        "spacing": 8
+      },
+
+      "disk": {
+        "format": "<span color='#${config.stylix.base16Scheme.base08}'>[]</span> {free}",
+        "interval": 20
+      },
+
+      "cpu": {
+        "format": "<span color='#${config.stylix.base16Scheme.base09}'>[]</span> {usage}%",
+        "tooltip": false,
+        "interval": 1
+      },
+
+      "custom/cpu_temp": {
+        "exec": "~/.config/waybar/scripts/waybarTemp.sh",
+        "return-type": "json",
+        "interval": 2,
+        "format": "{}",
+        "tooltip": false
+      },
+
+      "backlight": {
+        // "device": "acpi_video1",
+        "format": "<span color='#${config.stylix.base16Scheme.base0A}'>[󰞏]</span> {percent}%",
+        "tooltip": false,
+        "on-click-right": "~/.config/waybar/scripts/wlsunset.sh"
+      },
+
+      "custom/memory": {
+        "exec": "~/.config/waybar/scripts/memory_usage.sh",
+        "interval": 2,
+        "return-type": "json",
+        "format": "<span color='#${config.stylix.base16Scheme.base0C}'>[]</span> {}"
+      },
+
+      "pulseaudio": {
+        // "scroll-step": 1, // %, can be a float
+        "format": "<span color='#${config.stylix.base16Scheme.base0D}'>[]</span> {volume}%",
+        "format-muted": "<span color='#${config.stylix.base16Scheme.base08}'>[]</span> {volume}%",
+        "format-bluetooth": "<span color='#${config.stylix.base16Scheme.base0D}'>[󰂰]</span> {volume}%",
+        "format-bluetooth-muted": "<span color='#${config.stylix.base16Scheme.base08}'>[󰂲]</span> {volume}%",
+        "format-source": "{volume}% ",
+        "on-click": "pactl set-sink-mute @DEFAULT_SINK@ toggle",
+        "on-click-right": "blueman-manager",
+        "tooltip": false,
+        "max-volume": 130
+      },
+
+      "battery#bat2": {
+        "bat": "BAT2"
+      },
+
+      "battery": {
+        "interval": 1,
+        "states": {
+          "good": 99,
+          "warning": 30,
+          "critical": 20
+        },
+        "format-icons": [
+          "󰂎",
+          "󰁺",
+          "󰁻",
+          "󰁽",
+          "󰁾",
+          "󰁿",
+          "󰂀",
+          "󰂁",
+          "󰂂",
+          "󰁹"
+        ],
+
+        "format": "<span color='#${config.stylix.base16Scheme.base0B}'>[{icon}]</span> {capacity}%",
+        "format-full": "<span color='#${config.stylix.base16Scheme.base0B}'>[{icon}]</span> {capacity}%",
+        "format-plugged": "<span color='#${config.stylix.base16Scheme.base0B}'>[󰂅]</span> {capacity}%",
+
+        "format-charging": "<span color='#${config.stylix.base16Scheme.base0B}'>[󰂅]</span> {capacity}%",
+        "format-charging-warning": "<span color='#${config.stylix.base16Scheme.base09}'>[󰢝]</span> {capacity}%",
+        "format-charging-critical": "<span color='#${config.stylix.base16Scheme.base08}'>[󰢜]</span> {capacity}%",
+
+        "format-warning": "<span color='#${config.stylix.base16Scheme.base09}'>[{icon}]</span> {capacity}%",
+        "format-critical": "<span color='#${config.stylix.base16Scheme.base08}'>[{icon}]</span> {capacity}%!!",
+
+        "format-alt": "<span color='#${config.stylix.base16Scheme.base0B}'>[󱧥]</span> {time}",
+
+        "tooltip": false
+      },
+
+      "clock": {
+        "format": "<span color='#${config.stylix.base16Scheme.base0A}'>[]</span> {:%a %d | %I:%M %p}",
+        "tooltip": false,
+        "interval": 1
+      },
+
+      "network": {
+        "interval": 2,
+        "format": "<span color='#${config.stylix.base16Scheme.base0E}'>[󱘖]</span> {bandwidthDownBits}",
+        "format-wifi": "<span color='#${config.stylix.base16Scheme.base0E}'>[{icon}]</span> {bandwidthDownBits}",
+        "format-ethernet": "<span color='#${config.stylix.base16Scheme.base0E}'>[󰈀]</span> {bandwidthDownBits}",
+        "format-icons": [
+          "󰤫",
+          "󰤟",
+          "󰤢",
+          "󰤥",
+          "󰤨"
+        ],
+        "tooltip": false,
+        "states": {
+          "normal": 25
+        }
+      }
+    }
+  '';
+}
--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -3,17 +3,22 @@
   pkgs,
   ...
 }: {
+
+imports = [
+  ./configs/sway-jsonc.nix
+];
+
   programs.waybar.enable = true;
 
   # Install the actual config files from this directory
   xdg.configFile."waybar/hyprland.jsonc".source = ./hyprland.jsonc;
-  xdg.configFile."waybar/niri.jsonc".source = ./niri.jsonc;
-  xdg.configFile."waybar/sway.jsonc".source = ./sway.jsonc;
   xdg.configFile."waybar/hyprland.css".source = ./hyprland.css;
+  xdg.configFile."waybar/niri.jsonc".source = ./niri.jsonc;
   xdg.configFile."waybar/niri.css".source = ./niri.css;
+  # xdg.configFile."waybar/sway.jsonc".source = ./sway.jsonc;
   xdg.configFile."waybar/sway.css".source = ./sway.css;
 
-  xdg.configFile."scripts" = {
+  xdg.configFile."waybar/scripts" = {
     source = ./scripts;
     recursive = true;
   };
@@ -23,10 +28,36 @@
     @define-color background                  #${config.stylix.base16Scheme.base00};
     @define-color border                      #${config.stylix.base16Scheme.base03};
     @define-color text                        #${config.stylix.base16Scheme.base05};
+
+    /* workspaces */
     @define-color ws-focused                  #${config.stylix.base16Scheme.base0A};
-    @define-color ws-active-text              #${config.stylix.base16Scheme.base05};
+    @define-color ws-active-text              #${config.stylix.base16Scheme.base0B};
     @define-color ws-persistent-text          #${config.stylix.base16Scheme.base07};
     @define-color ws-urgent                   #${config.stylix.base16Scheme.base08};
+
+    /* semantic colors */
+    @define-color warning                     #${config.stylix.base16Scheme.base09};
+    @define-color critical                    #${config.stylix.base16Scheme.base08};
+    @define-color muted                       #${config.stylix.base16Scheme.base03};
+
+    /* module borders / accents */
+    @define-color window-border               #${config.stylix.base16Scheme.base0E};
+    @define-color tray-border                 #${config.stylix.base16Scheme.base02};
+    @define-color tray-bg                     #${config.stylix.base16Scheme.base01};
+    @define-color disk-border                 #${config.stylix.base16Scheme.base08};
+    @define-color cpu-border                  #${config.stylix.base16Scheme.base09};
+    @define-color temp-border                 #${config.stylix.base16Scheme.base0A};
+    @define-color backlight-border            #${config.stylix.base16Scheme.base0A};
+    @define-color memory-border               #${config.stylix.base16Scheme.base0C};
+    @define-color audio-border                #${config.stylix.base16Scheme.base0D};
+    @define-color battery-border              #${config.stylix.base16Scheme.base0B};
+    @define-color clock-border                #${config.stylix.base16Scheme.base0A};
+    @define-color network-border              #${config.stylix.base16Scheme.base0E};
+
+    /* mode indicator */
+    @define-color mode-bg                     #${config.stylix.base16Scheme.base0D};
+    @define-color mode-border                 #${config.stylix.base16Scheme.base0C};
+    @define-color mode-text                   #${config.stylix.base16Scheme.base00};
   '';
   #
   # systemd.user.services.waybar = {
