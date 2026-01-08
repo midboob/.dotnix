ea7914eb1ba9621037058ed3b12a2f4d472de1b6
midboob <edwarddan72@gmail.com>
2026-01-08 14:04:45 -0800

    feat: ✨ kitty fix

--- configs/home-manager/modules/terminals/kitty/default.nix
+++ configs/home-manager/modules/terminals/kitty/default.nix
@@ -33,7 +33,6 @@
       window_padding_width = "10";
       hide_window_decorations = "yes";
       # background_opacity = lib.mkForce "0.8";
-      background_opacity = "1";
       dynamic_background_opacity = "yes";
       confirm_os_window_close = "0";
       selection_foreground = "none";
