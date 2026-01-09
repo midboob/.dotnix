47a7730b96f0774dd5612be8470bcd0071661c8a
midboob <edwarddan72@gmail.com>
2026-01-09 15:03:25 -0800

    feat: ✨ changed some rars settings

--- configs/home-manager/modules/rars/default.nix
+++ configs/home-manager/modules/rars/default.nix
@@ -1,21 +1,20 @@
-{ pkgs, ... }:
-
-let
+{pkgs, ...}: let
   RARS = ./myapp.jar;
 in {
   home.packages = [
     (pkgs.writeShellScriptBin "RARS" ''
+      export GTK_APPLICATION_PREFER_DARK_THEME=0
+      unset GTK_THEME
+      unset QT_STYLE_OVERRIDE
       exec ${pkgs.jre}/bin/java -jar ${RARS} "$@"
     '')
   ];
 
-xdg.desktopEntries.myapp = {
-  name = "RARS 1.55";
-  comment = "My Java application";
-  exec = "RARS";
-  terminal = false;
-  categories = [ "Utility" ];
-};
-
-
+  xdg.desktopEntries.myapp = {
+    name = "RARS 1.55";
+    comment = "My Java application";
+    exec = "RARS";
+    terminal = false;
+    categories = ["Utility"];
+  };
 }
